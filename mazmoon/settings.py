1da9306272454d85684c9c4dd4f6d09db670f2c8
MSD <dadamuhames@gmail.com>
2023-02-19 21:39:56 +0500

    second commit

--- mazmoon/settings.py
+++ mazmoon/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uxxlg%zvt+np@(unak4#+n!=nld*fo(7nz+-u7dis9#8u*_yxe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mazmoongroup.pythonanywhere.com']
 
 
 # Application definition
